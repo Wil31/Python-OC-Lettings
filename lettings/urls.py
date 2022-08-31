c296d0fdae56ed53af79d26d2ce6a887b7dc5f51
Wil31 <w.gavigniaux@gmail.com>
2022-08-31 19:25:30 +0200

    Create namespaces for urls

--- lettings/urls.py
+++ lettings/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "lettings"
 urlpatterns = [
-    path("", views.lettings_index, name="lettings_index"),
+    path("", views.index, name="lettings_index"),
     path("<int:letting_id>/", views.letting, name="letting"),
 ]
