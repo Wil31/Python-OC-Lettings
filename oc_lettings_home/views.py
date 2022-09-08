47ad5ac6ac46c7082eda0f48bc55c72425a25f9a
Wil31 <w.gavigniaux@gmail.com>
2022-09-08 17:13:33 +0200

    insert line too long

--- oc_lettings_home/views.py
+++ oc_lettings_home/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 # consectetur ullamcorper non id est. Praesent dictum, nulla eget feugiat sagittis, sem mi
 # convallis eros, vitae dapibus nisi lorem dapibus sem. Maecenas pharetra purus ipsum, eget
 # consequat ipsum lobortis quis. Phasellus eleifend ex auctor venenatis tempus.
-# Aliquam vitae erat ac orci placerat luctus. Nullam elementum urna nisi, pellentesque iaculis enim
+# Aliquam vitae erat ac orci placerat luctus. Nullam elementum urna nisi, pellentesque iacu  lis enim
 # cursus in. Praesent volutpat porttitor magna, non finibus neque cursus id.
 def index(request):
     return render(request, "index.html")
