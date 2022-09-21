ddd55ac0c3d1d917216bcfe54c18f78545375719
Wil31 <w.gavigniaux@gmail.com>
2022-09-21 12:45:37 +0200

    Automatic configuration with django-on-heroku

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -109,8 +109,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-import django_heroku
+import django_on_heroku
 
-django_heroku.settings(locals())
+django_on_heroku.settings(locals())
