b1bfffc910113996d6ef0ff5e805947c6379c928
Wil31 <w.gavigniaux@gmail.com>
2022-09-21 16:38:56 +0200

    update settings

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,4 +1,5 @@
 import os
+import django_on_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -108,7 +109,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
-
-import django_on_heroku
 
 django_on_heroku.settings(locals())
