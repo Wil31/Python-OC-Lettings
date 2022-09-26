89c1fce0c97b0f3dcbd9f88a33b82a22ea6aa1b9
Wil31 <w.gavigniaux@gmail.com>
2022-09-26 17:50:52 +0200

    Add sentry configuration

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,6 +1,9 @@
 from django.core.management.utils import get_random_secret_key
 import os
 import django_on_heroku
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -115,3 +118,17 @@ django_on_heroku.settings(locals())
 
 CSRF_TRUSTED_ORIGINS = ["https://oc-lettings-docker-wil.herokuapp.com"]
 CSRF_COOKIE_SECURE = False
+
+sentry_sdk.init(
+    dsn=os.environ.get("SENTRY_DSN"),
+    integrations=[
+        DjangoIntegration(),
+    ],
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True,
+)
--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
 urlpatterns = [
+    path("sentry-debug/", trigger_error),
     path("", include("oc_lettings_home.urls", namespace="home")),
     path("lettings/", include("lettings.urls", namespace="lettings")),
     path("profiles/", include("profiles.urls", namespace="profiles")),
